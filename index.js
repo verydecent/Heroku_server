6fea961b0064150fba4df8006ee8b02bfa20ac9e
verydecent <wnjhwng@gmail.com>
2019-02-13 16:33:25 -0800

    adds start script

--- index.js
+++ index.js
@@ -2,6 +2,7 @@ require('dotenv').config();
 
 const server = require('./api/server.js');
 
+// Very important to make the port Dynamic
 const port = process.env.PORT || 8000;
 const instance = server.listen(port, () =>
 	console.log(`\n=== Server running on port: ${instance.address().port} ===\n`)
